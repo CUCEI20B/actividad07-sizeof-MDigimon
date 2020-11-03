0cb5165c282630446d251e3979a87444f5ef0e04
Jesus Eduardo Quintero Gomez <71619810+MDigimon@users.noreply.github.com>
2020-11-03 17:41:34 -0600

    Programa Terminado

--- main.cpp
+++ main.cpp
@@ -2,8 +2,62 @@
 
 using namespace std;
 
+string capturar(){
+	string entrada;
+	cout<<"Entrada: ";
+	cin>>entrada;
+	if(entrada=="int" 
+		|| entrada=="char" 
+		|| entrada=="short" 
+		|| entrada=="float" 
+		|| entrada=="double" 
+		|| entrada=="long"){
+		return entrada;
+	}	
+	else
+		return "0";
+}
+
+void imprimir(string entrada){
+	if(entrada=="int"){
+		cout<<"Bytes: "<<sizeof(int)<<endl<<endl;
+	}
+	
+	else if(entrada=="char"){
+		cout<<"Bytes: "<<sizeof(char)<<endl<<endl;
+	}
+	
+	else if(entrada=="short"){
+		cout<<"Bytes: "<<sizeof(short)<<endl<<endl;
+	}
+	
+	else if(entrada=="float"){
+		cout<<"Bytes: "<<sizeof(float)<<endl<<endl;
+	}
+	
+	else if(entrada=="double"){
+		cout<<"Bytes: "<<sizeof(double)<<endl<<endl;
+	}
+	
+	else{
+		cout<<"Bytes: "<<sizeof(long)<<endl<<endl;
+	}
+}
+
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+	int f=1;
+	string dato;
+	while(f==1){
+		dato=capturar();
+	    if(dato!="0"){
+	    	imprimir(dato);
+		}
+		else{
+			f=0;
+		}
+	}
+	cout<<endl<<"FIN DEL PROGRAMA"<<endl;
+    
 
     return 0;
 }
